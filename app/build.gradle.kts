09299a0225effa7c6e5977f2296d887ab431b4c8
redsun352 <greedyy3843@gmail.com>
2026-08-16 13:26:48 +0300

    Target Android 16 and compile SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.redsun352.gpsgrid"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.redsun352.gpsgrid"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
